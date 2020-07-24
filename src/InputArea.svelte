55b7227d09f1c3b5f16f792fa6e9ed069b5943c8
Mathis Bullinger <mathis@bullinger.dev>
2020-07-25 00:07:51 +0200

    feat: input editor

--- src/InputArea.svelte
+++ src/InputArea.svelte
@@ -1,14 +1,33 @@
 <script lang="ts">
+  let html = ''
 
+  const decodeHTML = (text) => {
+    const e = document.createElement('div')
+    e.innerHTML = text
+    return e.childNodes.length ? e.childNodes[0].nodeValue : ''
+  }
+
+  $: lines = html
+    .split(/<\/?(div|br)\/?>/)
+    .filter((v, i) => !(i % 2) && v)
+    .map((v) => decodeHTML(v.replace(/\<\/?\w+\/?\>/g, '')))
+
+  $: if (lines) {
+    console.log(lines)
+  }
 </script>
 
 <style>
   div {
     display: block;
+    font-family: inherit;
+    margin: 0;
     height: 100%;
-    padding: max(5vmin, 1rem);
     width: calc(80% + var(--resize-offset));
     min-width: 30ch;
+    --padding: max(5vmin, 1rem);
+    padding-top: var(--padding);
+    padding-left: var(--padding);
   }
 
   div:focus {
@@ -16,4 +35,4 @@
   }
 </style>
 
-<div contenteditable="true" />
+<div contenteditable="true" bind:innerHTML={html} />
