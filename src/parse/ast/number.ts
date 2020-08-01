491034224c4152e380a6336d7f986ca037c64b8a
Mathis Bullinger <mathis@bullinger.dev>
2020-08-01 04:32:17 +0200

    feat: parse hexadecimal & binary

--- src/parse/ast/number.ts
+++ src/parse/ast/number.ts
@@ -2,8 +2,17 @@ import EvalNode from './evalNode'
 import type Token from '../token'
 
 export default class Number extends EvalNode<number> {
+  private readonly radix: number = 10
+  private readonly value: string
+
   constructor(readonly token: Token) {
     super()
+    const prefix = token.lexeme.slice(0, 2).toLowerCase()
+    if (/0[a-z]/.test(prefix)) {
+      if (prefix[1] === 'x') this.radix = 16
+      else if (prefix[1] === 'b') this.radix = 2
+      this.value = this.token.lexeme.slice(2)
+    } else this.value = this.token.lexeme
   }
 
   toString(): string {
@@ -11,6 +20,6 @@ export default class Number extends EvalNode<number> {
   }
 
   eval() {
-    return parseInt(this.token.lexeme)
+    return parseInt(this.value, this.radix)
   }
 }
